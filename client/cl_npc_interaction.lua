b2e3b3fb4cbdd77ed29342ed672469da655c922a
JustSuzuya <98284732+FakeSuzuya@users.noreply.github.com>
2024-08-30 15:22:50 +0200

    fix errors 🍇
    fix plusieurs erreurs , fix du fichier sql

--- client/cl_npc_interaction.lua
+++ client/cl_npc_interaction.lua
@@ -1,6 +1,8 @@
 local npcLocation = vector3(-1888.87, 2060.49, 140.98)
 local npcHeading = 340.0 -- Orientation du NPC
 local npcModel = 'a_m_m_farmer_01' -- Exemple de modèle de NPC
+local npcEntity -- Variable pour stocker l'entité du NPC
+local nuiActive = false -- Variable pour suivre l'état du NUI
 
 ESX = ESX or exports['es_extended']:getSharedObject()
 
@@ -12,22 +14,32 @@ CreateThread(function()
     end
 
     -- Créer le NPC
-    local npc = CreatePed(4, npcModel, npcLocation.x, npcLocation.y, npcLocation.z, npcHeading, false, true)
-    FreezeEntityPosition(npc, true) -- Le NPC reste en place
-    SetEntityInvincible(npc, true)  -- Le NPC est invincible
-    SetBlockingOfNonTemporaryEvents(npc, true) -- Empêche le NPC de réagir à son environnement
+    npcEntity = CreatePed(4, npcModel, npcLocation.x, npcLocation.y, npcLocation.z, npcHeading, false, true)
+    FreezeEntityPosition(npcEntity, true) -- Le NPC reste en place
+    SetEntityInvincible(npcEntity, true)  -- Le NPC est invincible
+    SetBlockingOfNonTemporaryEvents(npcEntity, true) -- Empêche le NPC de réagir à son environnement
 
     -- Ajouter l'interaction pour le NPC via ox_target
-    exports.ox_target:addLocalEntity(npc, {
+    exports.ox_target:addLocalEntity(npcEntity, {
         {
             name = 'npc_menu',
             label = _U('npc_menu_title'),
             icon = 'fa-solid fa-user',
             distance = 2.0,
             onSelect = function()
-                -- Ouvrir le menu NUI lors de l'interaction
-                SetNuiFocus(true, true)
-                SendNUIMessage({ action = 'openMenu' })
+                if not nuiActive then
+                    -- Vérifie si le joueur est à proximité du NPC avant d'ouvrir le menu
+                    local playerPed = PlayerPedId()
+                    local playerCoords = GetEntityCoords(playerPed)
+                    local npcCoords = GetEntityCoords(npcEntity)
+                    local distance = #(playerCoords - npcCoords)
+
+                    if distance <= 2.0 then
+                        SetNuiFocus(true, true)
+                        SendNUIMessage({ action = 'openMenu' })
+                        nuiActive = true -- Mettre à jour l'état NUI
+                    end
+                end
             end
         }
     })
@@ -49,6 +61,11 @@ RegisterNUICallback('menuAction', function(data, cb)
         ESX.TriggerServerCallback('vigneron:checkEmployees', function(employees)
             SendNUIMessage({ action = 'updateEmployeeList', employees = employees })
         end)
+    elseif data.action == 'close_menu' then
+        -- Fermer le menu NUI
+        SetNuiFocus(false, false)
+        SendNUIMessage({ action = 'closeMenu' })
+        nuiActive = false -- Réinitialiser l'état NUI
     end
     cb('ok')
 end)
